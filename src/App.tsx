1bc7ecc9a3ac62c9643d9a332877d6320171f5f4
anishgm07 <anishgm007@gmail.com>
2023-07-10 19:00:39 +0530

    add_web_design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import './styles/App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Footer from './components/Footer';
+import logo from './Images/logo/main_logo.png';
+import './styles/App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome To the page
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <header className="header">
+          <img src={logo} alt="Logo" className="logo" />
+          <Navbar />
+        </header>
+        <main className="main-content">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/contact" component={ContactPage} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,2 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components/NavBar.css'
 
-export {};
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+        <ul className="navbar_list">
+            <li className="navbar_item"><Link to="/" className="navbar_link">Home</Link></li>
+            <li className="navbar_item"><Link to="/about" className="navbar_link">About</Link></li>
+            <li className="navbar_item"><Link to="/contact" className="navbar_link">Contact</Link></li>
+        </ul>
+  </nav>
+  );
+}
+
+export default Navbar;
